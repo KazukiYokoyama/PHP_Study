1fac945829109d842fcab5eb866091c92ece4868
ky <4580kzk@gmail.com>
2019-01-27 03:38:42 +0000

    urlパラメータ取得でphpの警告が出る問題を修正。 また、Viewファイルの読み込みの仕方を変更し、全体のレイアウトとページのコンテンツを分けて記述できるように修正した。

--- html/index.php
+++ html/index.php
@@ -1,10 +1,14 @@
 <?php
 
 // urlパラメータ取得
-$params = explode('/', $_GET['url']);
+$url = filter_input(INPUT_GET, 'url');
+$params = explode('/', $url);
 
 // 最初のパラメータをリクエストしたページとする
 $page = array_shift($params);
+
+// ページの指定が無い場合、トップページを表示する
+if($page == ''){ $page = 'home'; }
 
 // Model読み込み
 include ('../models/'.$page.'.php');
@@ -14,6 +18,8 @@ $ret = handle($params);
 extract($ret);
 
 // View読み込み
-include ('../views/'.$page.'.php');
+include ('../views/'.$page.'.view.php');
+$page_print = new PagePrint();
+$page_print->PageView();
 
 ?>
