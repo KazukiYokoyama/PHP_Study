3bf55f95c33eb4739fc8de55c9061ecce6728a33
ky <4580kzk@gmail.com>
2019-01-28 07:02:03 +0000

    共通部品を公開画面のデフォルトレイアウトに移動

--- views/home.view.php
+++ views/home.view.php
@@ -1,1 +1,2 @@
 <h1>Blog Home</h1>
+<?= $str ?>
--- views/registered.view.php
+++ views/registered.view.php
@@ -1,23 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-            <a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-    </div>
-    <form class="form-inline">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-</nav>
 <div class="container">
-    <div class="text-center">
-        <h1>アカウント登録完了</h1>
-        <p>アカウントの登録が完了しました！</p>
-    </div>
+  <div class="text-center">
+    <h1>アカウント登録完了</h1>
+    <p>アカウントの登録が完了しました！</p>
+  </div>
 </div>
