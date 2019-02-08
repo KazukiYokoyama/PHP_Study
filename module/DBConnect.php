0d014eb4cf58c2f9637c720173b881ae3e54bab3
minami1059 <m1059y@gmail.com>
2019-02-08 00:33:41 +0000

    データベース接続処理の修正
    ・接続情報を定数で持たせないように変更
    ・接続失敗時にエラー内容をエラーログに書き込むように修正

--- module/DBConnect.php
+++ module/DBConnect.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * このファイル内だけでしか使用しない場合、定数宣言を行う旨味がなくなってしまう。
- * DBへの接続情報を再利用したいのであれば、別ファイルに記述するように変更しましょう。
- */
-define('HOST', 'localhost');
-define('DATABASE', 'Blogs');
-define('USER', 'phpstudy');
-define('PASSWORD', 'phpstudy');
-
 //###########################################
 // PDOによるデータベースへの接続を行う
 //###########################################
@@ -19,9 +10,9 @@ class DB_Connect{
         try{
             //データベースに接続
             $this->pdo = new PDO(
-                'mysql:dbname='.DATABASE.';host='.HOST.';charset=utf8mb4',
-                USER,
-                PASSWORD,
+                'mysql:dbname=Blogs;host=localhost;charset=utf8mb4',
+                'phpstudy',
+                'phpstudy',
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -31,12 +22,8 @@ class DB_Connect{
             //接続失敗した場合エラー内容をログに残す
             $msg = "データベースへの接続に失敗しました<br>(" . $e->getMessage() . ")";
             //エラーログに書き込む
-            /**
-             * ちゃんとエラーログに出力してからpullリクだしてください。
-             * 例）
-             * error_log("データベースへの接続に失敗しました(".$e->getMessage().")", 0);
-             * header("Location: /error");    // エラーページにリダイレクト
-             */
+            error_log("データベースへの接続に失敗しました(".$e->getMessage().")", 0);
+            header("Location: /error");    // エラーページにリダイレクト
         }
     }
 
