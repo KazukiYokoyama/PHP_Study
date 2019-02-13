063a00744dea2e1acfce4ae0a70543725148acd3
ky <4580kzk@gmail.com>
2019-02-13 16:48:25 +0000

    Modelでurlを取得できるよう修正

--- module/Controller.php
+++ module/Controller.php
@@ -26,19 +26,21 @@ Class Controller{
      * Modelの実行内容をViewに渡し、編集したページを表示する
      */
     public function Main(){
-
-        $Page = new Page();
+        $page_data = [];    // ページで使用する情報
 
         // Modelの処理を実行
         if(file_exists('../models/'.$this->page.'.php')){
-            include ('../models/'.$this->page.'.php');
+            include_once ('../models/'.$this->page.'.php');
 
             // Modelから実行結果を取得
-            $model = new $this->page;
-            $Page->setData( $model->getData() );
+            $model = new $this->page($this->url);
+            $model->Action();
+            $page_data = $model->getData();
         }
 
         // 編集したページを表示
+        $Page = new Page();
+        $Page->setData($page_data);
         $Page->Print();
     }
 }
--- module/Model.php
+++ module/Model.php
@@ -1,8 +1,22 @@
 <?php
 
 class Model{
-    protected $page_data = [];
-    public function getData() :array{
+    protected $url = [];        // リクエストされたURL
+    protected $page_data = [];  // ページで使用する情報
+
+    function __construct(array $url) {
+        $this->url = $url;
+    }
+
+    /**
+     * Modelの処理を記述する
+     */
+    public function Action(){}
+
+    /**
+     * ページで使用する情報を返す
+     */
+    final public function getData() :array{
         return $this->page_data;
     }
 }
