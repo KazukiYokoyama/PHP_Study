19313a313adfc44ed03001f95a40b96e7e005b15
minami1059 <m1059y@gmail.com>
2019-02-14 00:45:18 +0000

    ログイン処理の修正

--- models/login.php
+++ models/login.php
@@ -4,7 +4,23 @@
 //###########################################
 include ('../module/DBConnect.php');
 
+// セッションの有効期限を1時間に設定
+session_set_cookie_params(60 * 60);
+//セッションスタート
+session_start();
+
 class Login extends Model{
+    public function Login(){
+        //ログインボタンが押下された場合
+        if (isset($_POST["login"])){
+            $login_check = new LoginCheck();
+            //エラーメッセージが返ってこなければログインの判定を行う
+            if(!$login_check->GetErrorMessage()){
+                $login_check->LoginDecision();
+            }
+        }
+    } 
+
     protected $page_data = [
         'page' => 'login',
         'layout' => 'public_default',
@@ -12,64 +28,74 @@ class Login extends Model{
     ];
 }
 
-// セッションの有効期限を1時間に設定
-session_set_cookie_params(60 * 60);
+/**
+ * ログインの判定を行う
+ */
+class LoginCheck{
+    private $email;
+    private $password;
+    private $errorMessage = [];
 
-//セッションスタート
-session_start();
-
-//エラーメッセージの初期化
-$errorMessage = '';
-
-//ログインボタンが押下された場合
-if(isset($_POST["login"])){
-    //入力の有無をチェック
-    if(empty($_POST["email"])){
-        $errorMessage = 'メールアドレスを入力してください。';
+    //初期処理
+    function __constract(){
+        $this->email = $_POST['email'];
+        $this->password = $_POST['password'];
+        //入力チェック
+        InputCheck();
     }
-    if(empty($_POST["password_hash"])){
-        $errorMessage = 'パスワードを入力してください';
-    }
-
-    //メールの形式のチェック
-    if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-        $errorMessage = 'メールアドレスの形式が正しくありません。';
-    }
-    //パスワードの形式チェック
-    if(!preg_match("/^[a-zA-Z0-9]+$/", $$_POST["password_hash"])){
-        $errorMessage = 'パスワードの形式が正しくありません。';
-    }
-
-    if(!empty($_POST["email"]) && !empty($_POST["password_hash"])){
-        $email = $_POST["email"];
-        $password = $_POST["password_hash"];
-
-        try{
-             //データベース接続
-            $DB = new DB_Connect();
-            $stmt = $DB->prepare('SELECT * FROM Accounts WHERE email = :email');
-            $stmt->execute([':email'=>$email]);
-
-            if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                //パスワードのチェック
-                if(password_verify($password, $row["password_hash"])){
-                    //セッションIDの新規取得
-                    session_regenerate_id(true);
-
-                    //セッションにアカウントIDを格納
-                    $_SESSION["account_id"] = $row['account_id'];
-                    $_SESSION['login'] = true;
-
-                    //ホーム画面へ遷移
-                    header("Location: /models/home.php");
-                    exit();
-                }else{
-                    $errorMessage = 'ユーザーIDあるいはパスワードに誤りがあります。';
-                }
-            }
-        }catch(PDOException $e){
-            $errorMessage = 'データベースエラー';
+    
+    //入力チェック処理
+    public function InputCheck(){
+        //入力の有無をチェック
+        if(empty($this->email)){
+            $errorMessage = 'メールアドレスを入力してください。';
         }
+        if(empty($this->password)){
+            $errorMessage = 'パスワードを入力してください';
+        }
+    
+        //メールの形式のチェック
+        if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            $errorMessage = 'メールアドレスの形式が正しくありません。';
+        }
+        //パスワードの形式チェック
+        if(!preg_match("/^[a-zA-Z0-9]+$/", $this->password)){
+            $errorMessage = 'パスワードの形式が正しくありません。';
+        }
+    }
+
+    //DB接続し、ログインの判定を行う
+    public function LoginDecision(){
+        try{
+            //データベース接続
+           $DB = new DB_Connect();
+           $stmt = $DB->prepare('SELECT * FROM Accounts WHERE email = :email');
+           $stmt->execute([':email'=>$this->email]);
+
+           if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+               //パスワードのチェック
+               if(password_verify($this->password, $row["password_hash"])){
+                   //セッションIDの新規取得
+                   session_regenerate_id(true);
+
+                   //セッションにアカウントIDを格納
+                   $_SESSION["account_id"] = $row['account_id'];
+
+
+                   //ホーム画面へ遷移
+                   header("Location: /models/home.php");
+                   exit();
+               }else{
+                   $errorMessage = 'ユーザーIDあるいはパスワードに誤りがあります。';
+               }
+           }
+       }catch(PDOException $e){
+           $errorMessage = 'データベースエラー';
+       }
+    }
+    //エラーメッセージの取得
+    public function GetErrorMessage(){
+        return $this->$errorMessage;
     }
 }
 
