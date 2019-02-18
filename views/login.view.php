23c1502cf2c4070866f89807a883845a45d90b28
minami1059 <m1059y@gmail.com>
2019-02-18 01:11:38 +0000

    エラー文の書き出し処理を修正

--- views/login.view.php
+++ views/login.view.php
@@ -4,15 +4,14 @@
       <h1>ログイン画面</h1>
     </div>
     <form action="" method="post">
+    <?= $this->getData('errorMessage'); ?>
       <div class="form-group">
         <label for="exampleInputEmail1">Email address</label>
         <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="<?php if (!empty($_POST["email"])) {echo htmlspecialchars($_POST["email"], ENT_QUOTES);} ?>">
-        <?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?>
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
         <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        <?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?>
       </div>
       <input type="submit" name="login" class="btn btn-primary" value="login">
     </form>
