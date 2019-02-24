118cbf38f9b3f744248df274c9493dfdaf898817
vagrant <vagrant@localhost.localdomain>
2019-02-24 05:19:40 +0000

    アカウント登録画面：入力チェックエラー

--- models/register.php
+++ models/register.php
@@ -2,13 +2,110 @@
 //###########################################
 // Model　アカウント登録
 //###########################################
+include ('../module/DBConnect.php');
 
 class Register extends Model{
     protected $page_data = [
         'page' => 'register',
         'layout' => 'public_default',
-        'title' => 'アカウント登録'
+        'title' => 'アカウント登録',
+        'errorMessage' => ''
     ];
+
+    public function Action(){
+        // この中で登録処理を呼び出す
+        if (isset($_POST["insert"])){
+            
+            // 入力チェック
+            $input_check = new Account();
+
+            // エラーメッセージがあればそのメッセージを渡す
+            $err_msg = $input_check->Get_Errormessage();
+            
+            
+            // var_dump($err_msg);
+
+            // echo "<script>alert('エラー');</script>";
+            echo "<script>alert('$err_msg');</script>";
+
+        }
+    }
+
+}
+
+class Account {
+    private $username;                  // ユーザ名
+    private $email;                     // メールアドレス
+    private $password;                  // パスワード
+    private $errorMessage;              // エラーメッセージ
+
+    function __construct(){
+        // 画面から送信された内容を取得
+        var_dump($_POST['username']);
+        $this->username = $_POST['username'];
+        if(!isset($this->username)){
+            $this->username = '';
+        }
+        $this->email = $_POST['email'];
+        if(!isset($this->email)){
+            $this->email = '';
+        }
+        $this->password = $_POST['password'];
+        if(!isset($this->password)){
+            $this->password = '';
+        }
+        $this->Input_check();
+    }
+
+    protected function Accounts() {
+        // 入力チェック呼び出し
+        $Insert_Accounts = new Insert_Accounts();
+
+        // 登録処理
+
+
+    }
+
+    protected function Insert_Accounts(){
+        try{
+            //データベース接続
+           $DB = new DB_Connect();
+           $pdo = $DB->getPDO();
+           $stmt = $pdo->prepare('INSERT INTO Accounts (account_name, email, password_hash)VALUES(:account_name, :email, :password_hash)');
+           $stmt->execute([':account_name'=>$this->username]);
+           $stmt->execute([':email'=>$this->email]);
+           $stmt->execute([':password_hash'=>$this->password]);
+
+        }catch(PDOException $e){
+            $this->errorMessage = '<p>データベースエラー:'.$e->getMessage().'</p>';
+        }
+    }
+
+    protected function Input_check() {
+        $this->errorMessage="";
+        #ユーザ名：未入力チェック
+        if(empty($this->username)){
+            $this->errorMessage .= 'アカウント名を入力してください。\n';
+        }elseif(!preg_match("/^[a-zA-Z0-9]+$/", $this->username)){
+            $this->errorMessage .= 'アカウント名の形式が正しくありません。\n';
+        }
+        #Ｅメール：未入力チェック
+        if(empty($this->email)){
+            $this->errorMessage .= 'メールアドレスを入力してください。\n';
+        }elseif(!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            $this->errorMessage .= 'メールアドレスの形式が正しくありません。\n';
+        }
+        #パスワード：未入力チェック
+        if(empty($this->password)){
+            $this->errorMessage .= 'パスワードを入力してください。\n';
+        }elseif(!preg_match("/^[a-zA-Z0-9]+$/", $this->password)){
+            $this->errorMessage .= 'パスワードの形式が正しくありません。\n';
+        }
+    }
+
+    function Get_Errormessage(){
+        return $this->errorMessage;
+    }
 }
 
 ?>
--- views/register.view.php
+++ views/register.view.php
@@ -3,25 +3,25 @@
     <div class="text-center">
       <h1>アカウント登録</h1>
     </div>
-    <form>
+    <form method="POST">
       <div class="form-group">
-          <label for="exampleInputUserName">User Name</label>
-          <input type="text" class="form-control" id="exampleInputUserName" placeholder="Enter UserName">
+          <label for="username">User Name</label>
+          <input type="text" class="form-control" id="username" placeholder="Enter UserName">
       </div>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Password">
       </div>
       <div class="form-group form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" name="insert" class="btn btn-primary" value="insert">Submit</button>
     </form>
   </div>
 </div>
