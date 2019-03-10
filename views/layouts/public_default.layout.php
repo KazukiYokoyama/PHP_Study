2ddd95e8e049392a2d4e985435bac8da2c825eca
minami1059 <m1059y@gmail.com>
2019-03-10 08:53:09 +0000

    ログイン状態か否かで表示するメニューを切り替える

--- views/layouts/public_default.layout.php
+++ views/layouts/public_default.layout.php
@@ -23,15 +23,41 @@
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNav">
 				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/register.php">アカウント登録 <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/login.php">ログイン <span class="sr-only">(current)</span></a>
-					</li>
+					<?php
+						if ($this->getData('page') == 'home') {
+							echo '<li class="nav-item active"><a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a></li>';
+						} else {
+							echo '<li class="nav-item"><a class="nav-link" href="/home">Home</a></li>';
+						}
+					?>
+					<?php
+						//セッションにaccount_idがあればログインしている状態なので、表示するメニューを切り替える
+						if ($_SESSION["account_id"]) {
+							if($this->getData('page') == 'user'){
+								echo '<li class="nav-item active"><a class="nav-link" href="/user">マイページ <span class="sr-only">(current)</span></a></li>';
+							} else {
+								echo '<li class="nav-item"><a class="nav-link" href="/user">マイページ</a></li>';
+							}
+
+							if($this->getData('page') == 'logout'){
+								echo '<li class="nav-item active"><a class="nav-link" href="/logout">ログアウト <span class="sr-only">(current)</span></a></li>';
+							} else {
+								echo '<li class="nav-item"><a class="nav-link" href="/logout">ログアウト</a></li>';
+							}
+						} else {
+							if($this->getData('page') == 'register'){
+								echo '<li class="nav-item active"><a class="nav-link" href="/register">アカウント登録 <span class="sr-only">(current)</span></a></li>';
+							} else {
+								echo '<li class="nav-item"><a class="nav-link" href="/register">アカウント登録</a></li>';
+							}
+
+							if($this->getData('page') == 'login'){
+								echo '<li class="nav-item active"><a class="nav-link" href="/login">ログイン <span class="sr-only">(current)</span></a></li>';
+							} else {
+								echo '<li class="nav-item"><a class="nav-link" href="/login">ログイン</a></li>';
+							}
+						} 
+					?>
 				</ul>
 			</div>
 			<form class="form-inline">
