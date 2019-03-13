98b9763ebd44c0d1b423202d7fb9d4db1ba72e30
ky <4580kzk@gmail.com>
2019-03-13 15:20:36 +0000

    改行コードとインデントの修正

--- views/registered.view.php
+++ views/registered.view.php
@@ -1,6 +1,6 @@
 <div class="container">
-  <div class="text-center">
-    <h1>アカウント登録完了</h1>
-    <p>アカウントの登録が完了しました！</p>
-  </div>
+	<div class="text-center">
+		<h1>アカウント登録完了</h1>
+		<p>アカウントの登録が完了しました！</p>
+	</div>
 </div>
