ed7b318805f96c7c2418590fbfcc802d046e359e
minami1059 <m1059y@gmail.com>
2019-04-04 00:01:09 +0000

    hidden要素の追加とHTMLの修正

--- views/article_edit.view.php
+++ views/article_edit.view.php
@@ -4,23 +4,26 @@
 			<div class="text-center">
 				<h2>記事編集画面</h2>
 			</div>
-			<form action="" method="post">
+			<form name="ArticleEditForm" action="" method="post">
+				<input type="hidden" name="article_id" value="<?= $this->getData('article_id') ?>">
 				<?= $this->getData('errorMessage'); ?>
 				<div class="form-group pt-3">
 					<label for="exampleInputTitle">タイトル<span class="text-danger text-outline small">必須</span></label>
-					<input type="title" name="title" class="form-control" id="exampleInputTitle" value="<?php if (!empty($_POST["title"])) {echo htmlspecialchars($_POST["title"], ENT_QUOTES);} ?>">
+					<input type="title" name="title" class="form-control" id="exampleInputTitle" value="<?= $this->getData('title') ?>">
+					<input type="hidden" name="title" value="<?= $this->getData('title') ?>">
 					<?= $this->getData('title_errorMessage'); ?>
 				</div>
 				<div class="form-group pt-3 pb-3">
 					<label for="textarea1">本文</label>
 					<textarea id="textarea1" name="body" class="form-control" rows="10"><?= $this->getData('body') ?></textarea>
+					<input type="hidden" name="body" value="<?= $this->getData('body') ?>">
 				</div>
 				<div class="form-group text-center">
 					<button type="submit" name="submit" class="btn btn-primary">登録</button>
 					<button type="submit" name="cancel" class="btn btn-Light ml-2 mr-2">キャンセル</button>
 				<?php
-					if ($_GET["article_id"]) {
-						echo '<button type="submit" name="delet" class="btn btn-danger">削除</button>';
+					if (isset($_GET["article_id"])) {
+						echo '<button type="submit" name="delete" class="btn btn-danger">削除</button>';
 					}
 				?>
 				</div>
