301454f3fbc9c5cb1d94f50096cba3f229846a2a
ky <4580kzk@gmail.com>
2019-04-10 06:50:17 +0100

    ログアウト機能を追加

---
+++ models/logout.php
@@ -0,0 +1,16 @@
+<?php
+//###########################################
+// Model　ログアウト
+//###########################################
+
+class Logout extends Model{
+
+    /**
+     * ログアウトの動作
+     * @return void
+     */
+    public function Action(){
+        session_destroy();
+        header("Location: /home");
+    }
+}
