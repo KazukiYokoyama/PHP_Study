c2165e6a74af18170e88001242d66521a7baa633
ky <4580kzk@gmail.com>
2019-04-23 02:39:02 +0100

    home画面のレイアウトを調整

--- views/home.view.php
+++ views/home.view.php
@@ -3,36 +3,38 @@
 		<div class="text-center">
 			<h1>Blog Home</h1>
 		</div>
-		<div>
-			<h2>投稿者</h2>
-			<ul id="Contributors">
-				<template v-if="ListCount">
-					<li v-for="item in list" :key="item.account_name">
-						<a v-bind:href="'/articles/'+item.account_name">{{ item.account_name }}</a>
-					</li>
-				</template>
-			</ul>
-		</div>
-		<div>
-			<table id="Articles">
-				<tr>
-					<th>投稿者</th>
-					<th>タイトル</th>
-				</tr>
-				<template v-if="ListCount">
-					<tr v-for="item in list" :key="item.article_id">
-						<td>{{ item.account_name }}</td>
-						<td>
-							<a v-bind:href="'/user/articleedit/'+item.article_id">{{ item.title }}</a>
-						</td>
-					</tr>
-				</template>
-				<template v-else>
+		<div class="row">
+			<div class="col-md-9 table-responsive">
+				<table id="Articles" class="table table-bordered">
 					<tr>
-						<td colspan="2">投稿された記事はありません。</td>
+						<th>記事</th>
+						<th width="200px">投稿者</th>
 					</tr>
-				</template>
-			</table>
+					<template v-if="ListCount">
+						<tr v-for="item in list" :key="item.article_id">
+							<td>
+								<a v-bind:href="'/user/articleedit/'+item.article_id">{{ item.title }}</a>
+							</td>
+							<td>{{ item.account_name }}</td>
+						</tr>
+					</template>
+					<template v-else>
+						<tr>
+							<td colspan="2">投稿された記事はありません。</td>
+						</tr>
+					</template>
+				</table>
+			</div>
+			<div class="col-md-3">
+				<h2>投稿者</h2>
+				<ul id="Contributors" class="list-group list-group-flush">
+					<template v-if="ListCount">
+						<li class="list-group-item" v-for="item in list" :key="item.account_name">
+							<a v-bind:href="'/articles/'+item.account_name">{{ item.account_name }}</a>
+						</li>
+					</template>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
